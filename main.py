098ce42d5f4210d098e6c351c8ca2573f40b7a08
Vitor Guilherme Antunes <vguilhermeantunes@gmail.com>
2023-06-11 18:44:31 -0300

    fix nomes endpoints, plural -> singular

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def get_db():
     finally:
         dbs.close()
 
+# Add uma posicao
 @app.post("/ativo", response_model=schemas.Ativo)
 def create_ativo(ativo: schemas.AtivoCreate, dbs: Session = Depends(get_db)):
     db_ativo = crud.get_ativo_by_name(dbs, ativo.papel)
@@ -25,15 +26,14 @@ def create_ativo(ativo: schemas.AtivoCreate, dbs: Session = Depends(get_db)):
 
     return crud.create_ativo(db_session=dbs, ativo=ativo)
 
-
-@app.get('/ativos', response_model=list[schemas.Ativo])
+@app.get('/ativo', response_model=list[schemas.Ativo])
 def read_ativos(skip: int = 0, limit: int = 100, dbs: Session = Depends(get_db)):
     ativos = crud.get_ativos(dbs, skip=skip, limit=limit)
     return ativos
 
 @app.post("/posicao", response_model=schemas.Posicao)
 def create_posicao(posicao: schemas.PosicaoCreate, dbs: Session = Depends(get_db)):
-    db_posicao = crud.get_posicao_by_name(dbs, posicao.papel)
+    db_posicao = crud.get_posicao_by_name(dbs, posicao.nome)
 
     if db_posicao:
         raise HTTPException(status_code=400, detail='Posicão já adicionada')
@@ -41,7 +41,7 @@ def create_posicao(posicao: schemas.PosicaoCreate, dbs: Session = Depends(get_db
     return crud.create_posicao(db_session=dbs, posicao=posicao)
 
 
-@app.get('/posicoes', response_model=list[schemas.Posicao])
+@app.get('/posicao', response_model=list[schemas.Posicao])
 def read_posicao(skip: int = 0, limit: int = 100, dbs: Session = Depends(get_db)):
     ativos = crud.get_posicoes(dbs, skip=skip, limit=limit)
     return ativos
